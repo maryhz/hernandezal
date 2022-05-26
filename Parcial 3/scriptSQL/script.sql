d8225e2ad2d32a931d36a1ece60dcc7397ab88a7
maryhz <meryblack60@gmail.com>
2022-05-26 09:41:18 -0500

    actualización de script sql

--- Parcial 3/scriptSQL/script.sql
+++ Parcial 3/scriptSQL/script.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE peliculasDB;
+USE DATABASE peliculasDB;
 
-USE peliculasDB;
 CREATE TABLE peliculas(
 id_pelicula int(10) not null auto_increment,
 titulo varchar(50) not null,
@@ -14,6 +14,6 @@ audio varchar(50) not null,
 primary key(id_pelicula)
 );
 
-INSERT INTO peliculas VALUES(1,"Harry Potter y la piedra filosofal", 10, "153 minutos", "Harry Potter es el niño que vivió y debe enfrentar nuevas aventuras", "David Yates","PG-13","Fantasía","Español Latino");
-INSERT INTO peliculas VALUES(2,"Iron Man", 10, "142 minutos", "Tony Stark es un inventor de armamento brillante...", "Jon Favreau","PG-13","Película de superhéroes","Español Latino");
-INSERT INTO peliculas VALUES(3,"Train to Busan", 10, "160 minutos", "Un virus convierte a la gente en Zombies", "Yeon Sang-ho","NC-17","Acción","Coreano");
+INSERT INTO peliculas (titulo, raiting, duracion, reseña, director, clasificacion, generos, audio) VALUES("Harry Potter y la piedra filosofal", 10, "153 minutos", "Harry Potter es el niño que vivió y debe enfrentar nuevas aventuras", "David Yates","PG-13","Fantasía","Español Latino");
+INSERT INTO peliculas (titulo, raiting, duracion, reseña, director, clasificacion, generos, audio) VALUES("Iron Man", 10, "142 minutos", "Tony Stark es un inventor de armamento brillante...", "Jon Favreau","PG-13","Película de superhéroes","Español Latino");
+INSERT INTO peliculas (titulo, raiting, duracion, reseña, director, clasificacion, generos, audio) VALUES("Train to Busan", 10, "160 minutos", "Un virus convierte a la gente en Zombies", "Yeon Sang-ho","NC-17","Acción","Coreano");
