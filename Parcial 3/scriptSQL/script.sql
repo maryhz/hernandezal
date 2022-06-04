8406db08a89b1dfbe05b5d5ab5c7aab5d3f72d76
maryhz <meryblack60@gmail.com>
2022-06-03 20:50:07 -0500

    cambios de la BD

--- Parcial 3/scriptSQL/script.sql
+++ Parcial 3/scriptSQL/script.sql
@@ -6,7 +6,7 @@ id_pelicula int(10) not null auto_increment,
 titulo varchar(50) not null,
 raiting int(2) not null,
 duracion varchar(50) not null,
-reseña varchar(100) not null,
+resena varchar(100) not null,
 director varchar(50) not null,
 clasificacion varchar(20) not null,
 generos varchar(100) not null,
@@ -17,3 +17,5 @@ primary key(id_pelicula)
 INSERT INTO peliculas (titulo, raiting, duracion, reseña, director, clasificacion, generos, audio) VALUES("Harry Potter y la piedra filosofal", 10, "153 minutos", "Harry Potter es el niño que vivio y debe enfrentar nuevas aventuras", "David Yates","PG-13","Fantasia","Español Latino");
 INSERT INTO peliculas (titulo, raiting, duracion, reseña, director, clasificacion, generos, audio) VALUES("Iron Man", 10, "142 minutos", "Tony Stark es un inventor de armamento brillante...", "Jon Favreau","PG-13","Pelicula de superheroes","Español Latino");
 INSERT INTO peliculas (titulo, raiting, duracion, reseña, director, clasificacion, generos, audio) VALUES("Train to Busan", 10, "160 minutos", "Un virus convierte a la gente en Zombies", "Yeon Sang-ho","NC-17","Accion","Coreano");
+
+select *from peliculas;
